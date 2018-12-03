fa3195b099a32f578cf215bcf617ccc2f610a122
Ying Chen <jun.chenying@hotmail.com>
2018-12-03 16:53:52 +1100

    Loading Images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.jpg';
 
 function component() {
     let element = document.createElement('div');
@@ -7,7 +8,13 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
     
+    // Add the image to our existing div.
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
     return element;
-  }
+}
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
